cec1bc9852731542595227d5f2717b75fe6edba0
TheerawuthPat <trw.dev@gmail.com>
2025-08-20 13:30:12 +0700

    [Section2] Flutter & Dart Basic Completed

---
+++ lib/gradient_container.dart
@@ -0,0 +1,33 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:first_app/styled_text.dart';
+import 'package:flutter/material.dart';
+
+var startAligment = Alignment.topLeft;
+var endAligment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple()
+    : color1 = Colors.deepPurple,
+      color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAligment,
+          end: endAligment,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller()
+      ),
+    );
+  }
+}
